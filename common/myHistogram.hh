7f6a23e341263340343c1ccb5f4ee226d36eef02
Ouroboros42 <lucas.gathercole@gmail.com>
2024-01-19 20:31:07 +0000

    First commit. Basic histogram class

---
+++ common/myHistogram.hh
@@ -0,0 +1,64 @@
+#include <iostream>
+using namespace std;
+#include <cmath>
+
+
+// My rudimentary Histogram class 
+class myHistogram {
+ private:
+  int nbins;
+  float hi;
+  float lo;
+  float * hist;
+  int datsize;
+
+ public:
+  myHistogram(float * data, int ndat, int inNbins, float inLo, float inHi) {
+
+    //data = new float [sizeof(indata)];
+    nbins= inNbins;
+    lo = inLo;
+    hi = inHi;
+    
+    hist = new float[nbins];
+    // initialize histogram
+    for(int j=0; j<nbins; j++) {
+      hist[j]=0.0;
+    }
+    // do counting and create bins
+    float delta = (hi-lo)/nbins;
+    datsize = ndat;
+
+    // fill histogram ..
+    for(int k=0; k<datsize; k++) {
+      int bin_n = (data[k]-lo)/delta;
+      hist[bin_n] +=1;
+    }
+    
+  }
+
+
+  // print number of entries in ith bin
+    void printBinContents(int i){
+      std::cout <<  " Bin = " << i << " n_i = " <<  this->hist[i]<< std::endl;
+    }
+    
+    // fill in all the bins
+    void printAllBins() {
+      for(int i=0; i<nbins; i++) {
+	      std::cout << " Entries in bin i = "<< i << " n_i = " << this->hist[i] << std::endl; 
+      }
+    }
+    
+    // print upper edge
+    void upperEdge(){
+      std::cout << " Upper edge " << this->hi << std::endl;
+    }
+    
+    // print lower edge
+    void lowerEdge(){
+      std::cout << " Lower edge " << this->lo << std::endl;
+    }
+ 
+};
+
