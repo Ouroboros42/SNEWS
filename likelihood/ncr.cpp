352d9b2354da29aabb4ccd97693267a199edd9e8
Ouroboros42 <lucas.gathercole@gmail.com>
2024-01-26 02:58:26 +0000

    Add nCr calculation

---
+++ likelihood/ncr.cpp
@@ -0,0 +1,16 @@
+#include <algorithm>
+#include <iostream>
+
+using namespace std;
+
+float choose(int n, int r) {
+    r = min(r, n-r); // Take advantage of nCr = nC(n-r)
+
+    // (n-r)! has been factored out of n!, product all remaining terms
+    float out = 1;
+    for (int denom = 1; denom <= r; denom++) {
+        out *= (n - denom + 1) / (float) denom;
+    }
+
+    return out;
+}
