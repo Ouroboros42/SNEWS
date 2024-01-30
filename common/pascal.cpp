a5a7574240abd1ec80b1f09bad4e5e524f34171b
Ouroboros42 <lucas.gathercole@gmail.com>
2024-01-30 22:34:11 +0000

    Remove redundant pascal file

--- common/pascal.cpp
+++ common/pascal.cpp
@@ -6,8 +6,8 @@ vector<vector<float>> pascal_rhombus(size_t n_rows, size_t n_cols) {
     vector<vector<float>> rhombus(n_rows, vector<float>(n_cols, 1)); // Initialise nxm array of 1s
 
     for (size_t y = 1; y < n_rows; y++) {
-        for (size_t x = 1; x < n_cols; x++) { // Loop over element in row
-            rhombus[y][x] = rhombus[y-1][x] + rhombus[y][x-1];
+        for (size_t x = 1; x < n_cols; x++) {
+            rhombus[y][x] = rhombus[y-1][x] + rhombus[y][x-1]; // Populate with recursion relation
         }
     }
     
