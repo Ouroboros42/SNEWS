fd2700bb38284a3c10fe347cf1f13b5cf5037e1c
Ouroboros42 <lucas.gathercole@gmail.com>
2024-02-03 21:42:03 +0000

    Add jsoncpp for reading data

---
+++ likelihood/src/entrypoints/tests/json.cpp
@@ -0,0 +1,14 @@
+#define CATCH_CONFIG_MAIN
+
+#include <catch.hpp>
+#include <json/json.h>
+#include <fstream>
+#include <iostream>
+
+TEST_CASE("Simple read") {
+    Json::Value SKData;
+    std::ifstream SKFile("../temp-data/nlog-dump-SK-json-121-0.json", std::ifstream::binary);
+    SKFile >> SKData;
+
+    REQUIRE_NOTHROW(SKData["truth"]["true_sn_ra"].asDouble());
+}
