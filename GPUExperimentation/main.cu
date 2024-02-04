f8a8a57da7a3b890d29ea485c581f22d28ffa75e
Vansh <vanshjjw@gmail.com>
2024-02-04 15:50:05 +0000

    GPU stuff

---
+++ GPUExperimentation/main.cu
@@ -0,0 +1,7 @@
+#include <iostream>
+#include "main.cuh"
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
---
+++ GPUExperimentation/main.cuh
@@ -0,0 +1,8 @@
+//
+// Created by vansh on 01-02-2024.
+//
+
+#ifndef MAIN_CUH
+#define MAIN_CUH
+
+#endif //MAIN_CUH
