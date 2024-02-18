ce816b3f2f99b7087ff8896768921cce17a9feb7
Vansh <vanshjjw@gmail.com>
2024-02-18 16:55:07 +0000

    ff

--- likelihood/src/analysis/likelihood_dist.py
+++ likelihood/src/analysis/likelihood_dist.py
@@ -7,9 +7,10 @@ import argparse
 
 parser = argparse.ArgumentParser("Likelihood_Analysis")
 parser.add_argument("source_file", type=str)
-args = parser.parse_args()
+# args = parser.parse_args()
+# data_file_path = args.source_file
 
-data_file_path = args.source_file
+data_file_path = "abc"
 with open(data_file_path) as data_file: 
     data = json.load(data_file)
 
@@ -23,18 +24,3 @@ h1 = data["Binned"]["Signal-1"]
 h2s = data["Binned"]["Signals-2"]
 window_width = data["Binned"]["Window-Width"]
 
-fig, ((ax11, ax21), (ax12, ax22), (ax13, ax23)) = plt.subplots(3, 2)
-
-bins = np.linspace(0, window_width, len(h1) + 1)
-def plot_signal_hist(ax, hist):
-    ax.set_xlim(0, window_width)
-    ax.stairs(hist, bins)
-
-plot_signal_hist(ax11, h2s[0])
-plot_signal_hist(ax12, h2s[1])
-plot_signal_hist(ax13, h2s[2])
-plot_signal_hist(ax21, h1)
-
-ax22.scatter(t, L)
-
-plt.show()
