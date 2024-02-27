1e17b07ef6573465783a1648c48df3a4b5a50f2e
Vansh <vanshjjw@gmail.com>
2024-02-27 21:31:45 +0000

    refactoring

--- likelihood/src/analysis/helperMethods/Likelihood_Fits_And_Maxima.py
+++ likelihood/src/analysis/helperMethods/Likelihood_Fits_And_Maxima.py
@@ -3,7 +3,8 @@ import matplotlib.pyplot as plt
 import json
 import copy
 
-def suppressNoiseWithMovingAverage(L_data, T_data, window_half_width):
+
+def cleanWithMovingAverage(L_data, T_data, window_half_width):
     numPoints = len(L_data)
     L_cleaned = []
     T_cleaned = []
@@ -15,9 +16,9 @@ def suppressNoiseWithMovingAverage(L_data, T_data, window_half_width):
     return L_cleaned, T_cleaned
 
 
-def cleanMovingAverageAndNoiseFilter(L_data, T_data, window_half_width, noise_bound_in_sigma = 1.0):
-    # clean data with moving average
-    L_cleaned, T_cleaned = suppressNoiseWithMovingAverage(L_data, T_data, window_half_width)
+def cleanWithNoiseFilter(L_data, T_data, window_half_width, noise_bound_in_sigma = 1.0):
+    # clean data with moving average before
+    L_cleaned, T_cleaned = cleanWithMovingAverage(L_data, T_data, window_half_width)
 
     L_data = L_data[window_half_width: -window_half_width]
     T_data = T_data[window_half_width: -window_half_width]
@@ -26,44 +27,46 @@ def cleanMovingAverageAndNoiseFilter(L_data, T_data, window_half_width, noise_bo
     sigma = np.std(deviation)
     bound = noise_bound_in_sigma * sigma
 
-    L_final = []
-    T_final = []
+    Likelihoods = []
+    Time_Diffs = []
     for i, dev in enumerate(deviation):
         if abs(dev) < bound:
-            L_final.append(L_data[i])
-            T_final.append(T_data[i])
+            Likelihoods.append(L_data[i])
+            Time_Diffs.append(T_data[i])
 
-    return L_final, T_final
+    return Likelihoods, Time_Diffs
 
 
-def findDataMaximaAndErrors(L_data, T_data, error_bound = 0.5, ax=None, True_T = None):
+def findRawDataMaximaAndError(L_data, T_data, error_bound = 0.5, True_Lag = None, ax: plt.Axes = None):
     max_L_position = np.argmax(L_data)
     max_L_value = L_data[max_L_position]
     best_Lag = T_data[max_L_position]
 
-    pos_left = max_L_position - 1
-    while L_data[pos_left] > max_L_value - error_bound and pos_left > 0:
-        pos_left -= 1
+    i = max_L_position - 1
+    while i >= 0 and (L_data[i] > max_L_value - error_bound):
+        i -= 1
 
-    pos_right = max_L_position + 1
-    while L_data[pos_right] > max_L_value - error_bound and pos_right < len(L_data):
-        pos_right += 1
+    j = max_L_position + 1
+    while j < len(L_data) and (L_data[j] > max_L_value - error_bound):
+        j += 1
+
+    assert i <= max_L_position <= j
 
     if ax:
         ax.plot(T_data, L_data, "o", label="Likelihood data points")
         ax.axvline(x=best_Lag, linestyle="--", label="Best Lag")
-        ax.axvline(x=T_data[pos_left], linestyle="--", label="Error Bound")
+        ax.axvline(x=T_data[i], linestyle="--", label="Error Bound")
         ax.axvline(x=T_data[pos_right], linestyle="--")
-        if True_T:
-            ax.axvline(x=True_T, linestyle="--", label="True T", color="black")
+        if True_Lag:
+            ax.axvline(x=True_Lag, linestyle="--", label="True T", color="black")
         ax.set_xlabel("Time difference (s)")
         ax.set_ylabel("Likelihood")
         ax.legend()
 
-    return best_Lag, T_data[pos_left], T_data[pos_right]
+    return best_Lag, T_data[i], T_data[j]
 
 
-def findErrorOnCurveRecursively(coefficients, points, error_bound, max_recurse):
+def findErrorOnCurveRecursively(coefficients, points, error_bound, max_recurse: int = 5):
     L_fitted = np.polyval(coefficients, points)
     max_L_position = np.argmax(L_fitted)
     max_L_value = L_fitted[max_L_position]
@@ -81,25 +84,35 @@ def findErrorOnCurveRecursively(coefficients, points, error_bound, max_recurse):
         return best_Lag, points_within_error_bound[0], points_within_error_bound[-1]
 
 
-def LikelihoodFitPolynomial(L_data, T_data, number_of_points_to_evaluate=1000,
-                            degree=9, error_bound = 0.5, ax=None, True_T = None, plot_raw_data = False):
+def LikelihoodFitPolynomial(L_data, T_data,
+                            number_of_points_to_evaluate = 1000,
+                            degree = 9,
+                            error_bound = 0.5,
+                            ax: plt.Axes = None,
+                            True_T = None,
+                            plot_raw_data = False
+                            ):
+
     coefficients = np.polyfit(T_data, L_data, degree)
     offset_points = np.linspace(T_data[0], T_data[-1], number_of_points_to_evaluate)
     L_fitted = np.polyval(coefficients, offset_points)
 
-    best_Lag, error_left, error_right = findErrorOnCurveRecursively(coefficients, copy.deepcopy(offset_points), error_bound, 5)
+    best, err1, err2 = findErrorOnCurveRecursively(coefficients, copy.deepcopy(offset_points), error_bound)
+    assert err1 <= best <= err2
 
     if ax:
         ax.plot(offset_points, L_fitted, label="Fitted Curve")
-        ax.axvline(x=best_Lag, linestyle="--", label="Best Lag")
-        ax.axvline(x=error_left, linestyle="--", label="Error Bound")
-        ax.axvline(x=error_right, linestyle="--")
+        ax.axvline(x=best, linestyle="--", label="Best Lag")
+        ax.axvline(x=err1, linestyle="--")
+        ax.axvline(x=err2, linestyle="--")
+
         if True_T:
             ax.axvline(x=True_T, linestyle="--", label="True T", color="black")
         if plot_raw_data:
             ax.plot(T_data, L_data, "o", label="Likelihood data points")
+
         ax.set_xlabel("Time difference (s)")
         ax.set_ylabel("Likelihood")
         ax.legend()
 
-    return best_Lag, error_left, error_right
+    return best, err1, err2
