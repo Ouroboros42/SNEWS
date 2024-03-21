Fixing error calculation

likelihood/src/analysis/AnalyseTrials.py
@@ -28,10 +28,10 @@ def yourFavouriteMethods(Likelihoods, TimeDifferences, True_Lag, methods_id: int
             res = fits.findRawDataMaximaAndError(Likelihoods, TimeDifferences, True_Lag, ax = ax if draw else None)
 
         case 2:
-            res = fits.polynomialFit(Likelihoods, TimeDifferences, True_Lag, ax = ax if draw else None, error_bound=1)
+            res = fits.polynomialFit(Likelihoods, TimeDifferences, True_Lag, ax = ax if draw else None, error_bound=0.5)
 
         case 3:
-            hw = 3
+            hw = 1
             L_smoothed, T_smoothed = fits.smoothWithMovingAverage(Likelihoods, TimeDifferences, hw)
             res = fits.polynomialFit(L_smoothed, T_smoothed, True_Lag, ax = ax if draw else None)
 
@@ -126,7 +126,7 @@ def main(json_file):
 
     # analysis parameters (read between 0 - numTrials samples, and draw roughly 5-10 plots for visualisation)
     numSamplesToRead = numTrials
-    numPlotsToDraw = 5
+    numPlotsToDraw = 0
 
     # draw every n-th sample (set numPlotsToDraw = 0 above to disable)
     draw_every = (numSamplesToRead // numPlotsToDraw) if numPlotsToDraw > 0 else numTrials + 1
@@ -141,18 +141,6 @@ def main(json_file):
 
     # unpack and display results
     values, sigmas, score = unpackAndTestEstimates(estimates, True_Lag)
-
-    values = np.asarray(values)
-
-    b1, b2, b3 = biasTest.main(json_file, True_Lag, values, True_Lag, numTrials, out_folder)
-
-    errs = values - True_Lag
-
-    br1 = np.mean(b1 / errs)
-    br2 = np.mean(b2 / errs)
-    br3 = np.mean(b3 / errs)
-
-    print(f"{br1=}\n{br2=}\n{br3=}\n"
 
     help.display(True_Lag, values, sigmas, score, method_id, verbose=False, format_upto=2)
 

likelihood/src/analysis/helperMethods/Likelihood_Fits_And_Maxima.py
@@ -40,7 +40,7 @@ def smoothWithNoiseFilter(L_data, T_data, window_half_width, noise_bound_in_sigm
     return Likelihoods, Time_Diffs
 
 
-## ------------------- Raw Data Methods -------------------
+## ------------------- Legacy Methods -------------------
 ## Raw data analysis is not recommended because the error bounds are not reliable
 
 def findRawDataMaximaAndError(L_data, T_data, True_Lag, error_bound = 0.5, ax: plt.Axes = None):
@@ -81,11 +81,7 @@ def findRawDataMaximaAndError(L_data, T_data, True_Lag, error_bound = 0.5, ax: p
     return best_Lag, T_data[i], T_data[j]
 
 
-
-
-## ------------------- Curve Fitting and Error Methods -------------------
-
-def findErrorOnCurveRecursively(coefficients, points, error_bound, max_recurse: int = 5):
+def findErrorUsingDifferenceFromMaxValue(coefficients, points, error_bound, max_recurse: int = 5):
     L_fitted = np.polyval(coefficients, points)
     max_L_position = np.argmax(L_fitted)
     max_L_value = L_fitted[max_L_position]
@@ -98,9 +94,48 @@ def findErrorOnCurveRecursively(coefficients, points, error_bound, max_recurse: 
 
     if len(points_within_error_bound) < 2:
         points = np.linspace(points[max_L_position - 1], points[max_L_position + 1], 20)
-        return findErrorOnCurveRecursively(coefficients, points, error_bound, max_recurse - 1)
+        return findErrorUsingDifferenceFromMaxValue(coefficients, points, error_bound, max_recurse - 1)
     else:
         return best_Lag, points_within_error_bound[0], points_within_error_bound[-1]
+
+
+
+## ------------------- Curve Fitting and Error Methods -------------------
+
+
+def validationDebug(f, max, err1, err2):
+    print(f"Left {f(max) - f(err1)} and Right {f(max) - f(err2)}")
+
+
+def findErrorUsingSecondDerivative(coefficients, points, coeffs_errors, maxima_guess):
+    # info is Fisher's information. variance of the maximum likelihood estimator is 1/Fisher's information
+
+    # for c, error in zip(coefficients, coeffs_errors):
+    #     relative_error = error / c
+    #     print(f"Relative error : {relative_error * 100:.4f}%")
+
+
+    Likelihood = np.polynomial.polynomial.Polynomial(coef=coefficients[::-1])
+    Likelihood_prime = Likelihood.deriv()
+    Likelihood_double_prime = Likelihood_prime.deriv()
+
+    roots = Likelihood_prime.roots()
+    roots = [np.real(r) for r in roots if (r >= points[0] and r <= points[-1]) and np.isreal(r)]
+    roots = [r for r in roots if Likelihood_double_prime(r) < 0]
+
+    if len(roots) == 1:
+        closest_value = roots[0]
+    else:
+        closest_value = min(roots, key= lambda x : abs(x - maxima_guess))
+
+
+    info = -1 * Likelihood_double_prime(closest_value)
+    if info < 0:
+        print("WARNING: Second derivative is positive, but expected to be negative at the maximum")
+
+    # validationDebug(Likelihood, closest_value, closest_value - info, closest_value + info)
+    return closest_value, np.sqrt(1/np.abs(info))
+
 
 
 
@@ -111,18 +146,25 @@ def polynomialFit(
         L_data,
         T_data,
         True_Lag,
-        number_of_points_to_evaluate = 500,
+        number_of_points_to_evaluate = 1000,
         degree = 10,
         error_bound = 0.5,
         ax: plt.Axes = None,
-        plot_raw_data = True
+        plot_raw_data = True,
   ):
 
-    coefficients = np.polyfit(T_data, L_data, degree)
+    coefficients, cov = np.polyfit(T_data, L_data, degree, cov=True)
     offset_points = np.linspace(T_data[0], T_data[-1], number_of_points_to_evaluate)
     L_fitted = np.polyval(coefficients, offset_points)
 
-    best, err1, err2 = findErrorOnCurveRecursively(coefficients, copy.deepcopy(offset_points), error_bound)
+    # Legacy method based on function falling off from the maximum by 0.5
+    # best, err1, err2 = findErrorUsingDifferenceFromMaxValue(coefficients, copy.deepcopy(offset_points), error_bound)
+
+    # New method based on second derivative
+    maxima_guess = offset_points[np.argmax(L_fitted)]
+    error_on_coeffs = np.sqrt(np.diag(cov))
+    best, error = findErrorUsingSecondDerivative(coefficients, offset_points, error_on_coeffs, maxima_guess)
+    err1, err2 = best - error, best + error
 
     if ax:
         ax.plot(offset_points, L_fitted, label="Fitted Curve")